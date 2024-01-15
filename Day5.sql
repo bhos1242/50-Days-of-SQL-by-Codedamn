a1d2297a76bb37482c5973fc0b0b6e63259ec70d
vivek1242 <bhosvivek123@gmail.com>
2024-01-15 12:39:35 +0530

    Day 5 added

--- Day5.sql
+++ Day5.sql
@@ -1,3 +1,23 @@
+-- Schema
+-- patients table schema
+-- patient_id
+-- first_name
+-- last_name
+-- gender
+-- birth_date
+-- city
+-- province_id
+-- allergies
+-- height
+-- weight
+-- Task
+-- Write a SQL query to show all columns for patients who have specific patient_ids: 1, 45, 534, 879, 1000.
+
+-- Concepts to Know
+-- SELECT: To specify the columns you want to retrieve.
+-- WHERE: To filter the records based on certain conditions.
+-- IN: To specify multiple values in a WHERE clause.
+
 SELECT *
 FROM patients
 WHERE patient_id IN (1, 45, 534, 879, 1000);
